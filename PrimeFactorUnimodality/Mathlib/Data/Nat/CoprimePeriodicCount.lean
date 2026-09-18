47a678282a763c4f9a94b9759a94674fadb2fc1b
kimihiro64 <kimihiro64@users.noreply.github.com>
2026-09-18 05:40:42 +0000

    docs: attribute project work to Jonas Whidden

--- PrimeFactorUnimodality/Mathlib/Data/Nat/CoprimePeriodicCount.lean
+++ PrimeFactorUnimodality/Mathlib/Data/Nat/CoprimePeriodicCount.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Jonas Whidden. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: kimihiro64
+Authors: Jonas Whidden
 -/
 import Mathlib.Data.ZMod.Basic
 import PrimeFactorUnimodality.Mathlib.Data.Nat.Periodic
