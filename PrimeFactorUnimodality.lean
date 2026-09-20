c8e3b2e96c937315e72d9893a786047fe66cf15b
kimihiro64 <kimihiro64@users.noreply.github.com>
2026-09-20 19:40:59 +0000

    Wire compact Lemma 3.3 midrange into root

--- PrimeFactorUnimodality.lean
+++ PrimeFactorUnimodality.lean
@@ -1,6 +1,7 @@
 import PrimeFactorUnimodality.Assembly.FourthWitness
 import PrimeFactorUnimodality.Helpers.Analytic.MediumPNT
 import PrimeFactorUnimodality.Helpers.Analytic.DusartProof
+import PrimeFactorUnimodality.Helpers.Analytic.DusartLemma33Midrange
 import PrimeFactorUnimodality.Mathlib
 import PrimeFactorUnimodality.Proof.CompleteClassification
 import PrimeFactorUnimodality.Proof.Analytic.WangCrapisClosed
