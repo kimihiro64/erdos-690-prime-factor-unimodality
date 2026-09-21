7571b03ea43695bff898ef05f15b4f713de85e1c
kimihiro64 <kimihiro64@users.noreply.github.com>
2026-09-21 08:45:30 +0000

    Compile Table 6.6 endpoint interface with theta facade

--- PrimeFactorUnimodality/Helpers/Analytic/DusartThetaClosed.lean
+++ PrimeFactorUnimodality/Helpers/Analytic/DusartThetaClosed.lean
@@ -1,4 +1,5 @@
 import PrimeFactorUnimodality.Helpers.Analytic.DusartThetaClosedPart02
+import PrimeFactorUnimodality.Helpers.Analytic.FinitePrimeIntervalRows.Table66EndpointFacts
 
 set_option autoImplicit false
 
